b7955665bde7379d85020592695f828fb33046c8
Nico <nico.schwab@bluewin.ch>
2022-09-12 16:53:20 +0200

    Added area and angle calculations

--- exercises/A03.2/A03.2_solution.py
+++ exercises/A03.2/A03.2_solution.py
@@ -9,17 +9,14 @@ class Point:
         # 2. Speichert die Argumente x und y als öffentliche Attribute ab.
         self.x = x if isinstance(x, (int, float)) else 0
         self.y = y if isinstance(y, (int, float)) else 0
-        pass
 
     def get_x(self):
         # Gibt die x-Koordinate des Punktes zurück
         return self.x
-        pass
 
     def get_y(self):
         # Gibt die y-Koordinate des Punktes zurück
         return self.y
-        pass
 
     def get_xy(self):
         # Returns point as tuple
@@ -84,7 +81,6 @@ class Point:
             return math.sqrt(dx ** 2 + dy ** 2)
         else:
             raise ValueError
-        pass
 
     def get_angle(self, to_point):
         # 1. Prüft, ob to_point vom Typ Point ist
@@ -96,15 +92,28 @@ class Point:
             return math.degrees(math.atan2(to_point.y - self.y, to_point.x - self.x))
         else:
             raise ValueError
-        pass
+
+    def get_angle2points(self, p1, p2):
+        # 1. Prüft, ob p1 un p2 vom Typ Point sind
+        # 2. Falls ja: Berechnet den Winkel P1-SELF-P2 (also den Winkel, den die Strecken P1-SELF und SELF-P2 einfassen).
+        # 3. Falls nein: Führt 'raise ValueError' aus (ohne Anführungszeichen)
+        # 4. Gibt IMMER die positive Variante des Winkels zurück (z.B. wenn Winkel = -45 --> Rückgabewert = 360 - 45 = 315)
+        # Tipp 1: math.degrees(..) konvertiert von Radian zu Grad
+        # Tipp 2: math.atan2(delta_y, delta_x) rechnet den Arcus-Tangenz
+        if isinstance(p1, Point) and isinstance(p2, Point):
+            angle = self.get_angle(p2) - self.get_angle(p1)
+        else:
+            raise ValueError
+        if angle < 0:
+            return 360 + angle
+        else:
+            return angle
 
     @ staticmethod
     def get_rand_point():
         p = Point()
         p.randomize()
         return p
-
-    INT_MAX = 10000
 
     @staticmethod
     def onSegment(p, q, r) -> bool:
@@ -207,6 +216,10 @@ class Point:
 
 
 class Polygon:
+    class InvalidPolynomError(Exception):
+        def __init__(self, message):
+            super().__init__(message)
+
     INT_MAX = 1e21
 
     def __init__(self):
@@ -224,24 +237,27 @@ class Polygon:
             raise ValueError
         pass
 
-    def get_num_points(self):
-        # Gibt die Anzahl der eingegebenen Punkte zurück
+    def get_n(self):
+        # Gibt zurück, wie viele Ecken das Polygon hat
         return len(self.points)
-        pass
 
     def clear_points(self):
         # Löscht alle gespeicherten Punkte
         self.points = []
         pass
 
+    def get_sides(self):
+        # Gibt eine Liste mit allen Seiten(längen) zurück.
+        sides = []
+        for i, point in enumerate(self.points):
+            sides.append(point.get_distance(
+                self.points[(i + 1) % len(self.points)]))
+        return sides
+
     def get_circumference(self):
         # Berechnet den Umfang des Polygons
-        # (addiert also die Distanzen zw. P0 und P1, P1 und P2, ..., PN und P0)
-        circ = 0
-        for i, point in enumerate(self.points):
-            circ += point.get_distance(self.points[i - 1])
-        return circ
-        pass
+        # (Summiert alle Seitenlängen)
+        return sum(self.get_sides())
 
     def get_longest_side(self):
         # Findet die längste Seite des Polygons und gibt sie zurück
@@ -249,49 +265,91 @@ class Polygon:
         for i, point in enumerate(self.points):
             s.append(point.get_distance(self.points[i - 1]))
         return max(s)
-        pass
+
+    def get_inside_angles(self):
+        # Berechnet alle Innenwinkel des Polygons, also jeweils bei bei jedem Punkt der Winkel zw. den anliegenden Seiten,
+        # und gibt eine Liste mit all diesen Winkeln zurück.
+        # Tipp: Mit welcher Funktion lässt sich der Winkel zw. drei Punkten berechnen?
+        # Achtung: Je nach dem in welche Richtung das Polygon dreht kriegen Sie die Aussenwinkel.
+        # Mit der Formel sum(alle_winkel) = (n - 2) * 180 können Sie überprüfen, ob es sich um Innenwinkel handelt. Falls nicht, machen Sie daraus Innenwinkel.
+        angles = []
+        for i, point in enumerate(self.points):
+            angles.append(point.get_angle2points(
+                self.points[i - 1], self.points[(i + 1) % len(self.points)]))
+        if sum(angles) == (self.get_n() - 2) * 180:
+            # Sind Innenwinkel
+            return angles
+        else:
+            inv_angles = []
+            for angle in angles:
+                inv_angles.append(360 - angle)
+            if sum(inv_angles) == (self.get_n() - 2) * 180:
+                # Check if it's a non-complex polygon
+                return inv_angles
+            else:
+                # Has to be complex, so can't differ inner from outer angles
+                return None
+
+    def get_polygon_type(self):
+        # ZUSATZAUFGABE
+        # Überprüft, ob das Polygon regulär, konvex, konkav oder überschlagen (complex) ist.
+        # Regulär: Alle Seiten gleich lang und alle Winkel gleich gross.
+        # Konvex: Alle Innenwinkel sind <= 180 Grad
+        # Konkav: Mindestens ein Innenwinkel ist > 180 Grad
+        # Überschlagen: Funktion "get_inside_angles" gibt "None" zurück.
+        # return 0: regulär
+        #        1: konvex aber nicht regulär
+        #        2: konkav
+        #        3: überschlagen (complex)
+        # Schwierigkeit: EASY
+        angles = self.get_inside_angles()
+        if angles == None:
+            return 3  # überschlagen
+        for angle in angles:
+            if angle > 180:
+                return 2  # konkav
+        sides = self.get_sides()
+        if min(sides) == max(sides) and min(angles) == max(angles):
+            return 0  # regulär
+        else:
+            return 1  # konvex
 
     def has_unique_points(self):
         # ZUSATZAUFGABE
         # Überprüft, ob alle Punkte des Polygons einzigartig sind (also ob es keinen Punkt zwei Mal gibt)
-        # Schwierigkeit: MIDDLE
+        # Schwierigkeit: MEDIUM
         for i, point in enumerate(self.points):
             for j in range(i, len(self.points)):
                 if i != j:
                     if point == self.points[j]:
                         return False
         return True
-        pass
 
     def get_area(self):
         # ZUSATZAUFGABE
-        # Berechnet die Fläche des Polyons (angenommen, es ist ein reguläres Polygon)
-        # Tipp: Dreiecke
-        # Schwierigkeit: HARD
-        pass
+        # Berechnet die Fläche des Polyons
+        # Schwierigkeit: MEDIUM
+        # Tipp: Gaussche Trapezformel, geht für konvexe, koncave und komplexe Polygone.
+        sum = 0
+        for i in range(1, self.get_n()):
+            sum += self.points[i].y * (self.points[i - 1].x -
+                                       self.points[(i + 1) % len(self.points)].x)
+        return abs(0.5 * sum)
 
-    def is_regular(self):
-        # ZUSATZAUFGABE
-        # Überprüft, ob das Polygon ein reguläres Polygon ist
-        # Schwierigkeit: VERY HARD
-        pass
-
-    def is_inside(self, point):
-        pass
-
-        # Returns true if the point p lies
+    # Returns true if the point p lies
     # inside the polygon[] with n vertices
-    def is_inside(self, point) -> bool:
+
+    def is_inside(self, p) -> bool:
         # ZUSATZAUFGABE
         # Source: https://www.geeksforgeeks.org/how-to-check-if-a-given-point-lies-inside-a-polygon/
         # Überprüft, ob Punkt point innerhalb es Polygons liegt
         # Schwierigkeit: EXTREMELY HARD
 
-        if not isinstance(point, Point):
+        if not isinstance(p, Point):
             raise ValueError
 
         n = len(self.points)
-        p = point
+        p = p
 
         # There must be at least 3 vertices
         # in polygon
@@ -344,26 +402,53 @@ class Polygon:
         return (count % 2 == 1)
 
 
-viereck = Polygon()
-viereck.add_point(Point(0, 0))
-viereck.add_point(Point(0, 2))
-viereck.add_point(Point(2, 2))
-viereck.add_point(Point(2, 0))
+poly = Polygon()
+A = Point(0, 0)
+B = Point(2, 0)
+C = Point(2.5, 2.5)
+D = Point(0, 10.5)
+E = Point(-1, 2)
+poly.add_point(A)
+poly.add_point(E)
+poly.add_point(D)
+poly.add_point(C)
+poly.add_point(B)
 
-# print(viereck.get_circumference())
-# print(viereck.has_unique_points())
-# print(viereck.points[1] - viereck.points[2])
-# print(viereck.points[1] * 10)
+print(f"{poly.get_inside_angles() = }")
+print(f"{poly.is_convex() = }")
+print(f"{poly.get_area() = }")
 
-n = 1000
-n_eck = Polygon()
-for i in range(0, n):
-    n_eck.add_point(Point(0, i))
+complex_poly = Polygon()
+complex_poly.add_point(Point(4.2, 0.2))
+complex_poly.add_point(Point(5.8, 0.2))
+complex_poly.add_point(Point(4, 2))
+complex_poly.add_point(Point(5.8, 1.8))
 
-start = time.time()
-print(n_eck.has_unique_points())
-end = time.time()
-print(f"Took {round(end - start, 3)}s")
+# print(f"{complex_poly.get_inside_angles() = }")
+print(f"{complex_poly.get_area() = }")
+# print(poly.has_unique_points())
+# print(poly.points[1] - poly.points[2])
+# print(poly.points[1] * 10)
+
+# n = 1000
+# n_eck = Polygon()
+# for i in range(0, n):
+#     n_eck.add_point(Point(0, i))
+
+# start = time.time()
+# print(n_eck.has_unique_points())
+# end = time.time()
+# print(f"Took {round(end - start, 3)}s")
 
 
-print(f"Inside?: {viereck.is_inside(Point(2, 3))}")
+# print(f"Inside?: {poly.is_inside(Point(2.2, 1))}")
+
+
+# Testpunkt
+# print("### POINT-TESTS ###")
+# p1 = Point(0, 0)
+# p2 = Point(-1, 0)
+# p3 = Point(0, 1)
+# print(f"{p1.get_angle2points(p2,p3) = }")
+# print(f"{p3.get_angle2points(p1,p2) = }")
+# print(f"{p2.get_angle2points(p1,p3) = }")
